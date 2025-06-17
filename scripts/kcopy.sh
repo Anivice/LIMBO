28270b12f8688a34f19700a5541509c8c341786f
anivice <AniviceIves@gmail.com>
2025-06-17 16:27:43 +0800

    Added code/data space occupation display in the compiling stage

--- scripts/kcopy.sh
+++ scripts/kcopy.sh
@@ -13,6 +13,9 @@ data_size=$(stat "/tmp/data.${seed}.raw" --printf=%s)
 code_short=$((491136 - code_size))
 data_short=$((122752 - data_size))
 
+printf "Kernel code space usage %0.2f%% ($code_size bytes), data space usage %0.2f%% ($data_size bytes)\n" \
+    "$(echo "scale=4; $code_size / 491136 * 100" | bc)" "$(echo "scale=4; $data_size / 122752 * 100" | bc)"
+
 dd if=/dev/zero bs=$code_short count=1 >> "/tmp/code.${seed}.raw" 2>/dev/null
 dd if=/dev/zero bs=$data_short count=1 >> "/tmp/data.${seed}.raw" 2>/dev/null
 
